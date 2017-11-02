34f4dd2f13ba1ccab563f2fd9aff3b0c72eaa735
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-11-01 21:24:39 -0400

    completed bonus 1

--- exercise9.rb
+++ exercise9.rb
@@ -10,3 +10,20 @@ def display_cohort (cohort)
 end
 
 display_cohort students
+print students
+students[:cohort4] = 43
+puts students.keys
+students_update={}
+students.map do |cohort, number|
+  students_update[cohort] = number*1.05
+end
+
+students = students_update
+students.delete(:cohort2)
+puts students
+
+sum = 0
+students.each do |cohort, number|
+  sum += number
+end
+puts sum
